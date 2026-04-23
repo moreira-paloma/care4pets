c3bb02af093db028369dd6ec782375b177e8b137
Paloma Moreira <moreira_paloma@web.de>
2026-04-23 13:56:12 +0200

    feat: configurar Vite e arquivos iniciais do app

---
+++ care4pets-frontend-v2/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, Plugin } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { createServer } from "./server";
+
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    fs: {
+      allow: ["./client", "./shared", "index.html"],
+      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
+    },
+  },
+  build: {
+    outDir: "dist/spa",
+  },
+  plugins: [react(), expressPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client"),
+      "@shared": path.resolve(__dirname, "./shared"),
+    },
+  },
+}));
+
+function expressPlugin(): Plugin {
+  return {
+    name: "express-plugin",
+    apply: "serve", 
+    configureServer(server) {
+      const app = createServer();
+
+     
+      server.middlewares.use(app);
+    },
+  };
+}
