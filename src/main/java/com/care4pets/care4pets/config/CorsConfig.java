76f2b19375fede3cd3863accda3e9bf900d9dc2d
Paloma Moreira <moreira_paloma@web.de>
2026-06-17 08:13:27 +0200

    chore: ajustar CORS para produção

--- src/main/java/com/care4pets/care4pets/config/CorsConfig.java
+++ src/main/java/com/care4pets/care4pets/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins(
+                                "http://localhost:8081",
+                                "https://careforpets.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
